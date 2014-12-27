b85d112bc88794cf53a45e576de14726061db42b
TL Nguyen <tl.nguyen.1987@gmail.com>
2014-12-27 13:51:26 +0200

    Make Vehicle and Oil Parcelable

--- OilCheckr/app/src/main/java/eu/artviz/oilcheckr/models/Vehicle.java
+++ OilCheckr/app/src/main/java/eu/artviz/oilcheckr/models/Vehicle.java
@@ -1,8 +1,11 @@
 package eu.artviz.oilcheckr.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.j256.ormlite.field.DatabaseField;
 
-public class Vehicle {
+public class Vehicle implements Parcelable {
     @DatabaseField(generatedId = true)
     private int id;
     @DatabaseField
@@ -67,4 +70,39 @@ public class Vehicle {
     public void setAverageDayMileage(int averageDayMileage) {
         this.averageDayMileage = averageDayMileage;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeString(this.name);
+        dest.writeInt(this.currentMileage);
+        dest.writeParcelable(this.oil, flags);
+        dest.writeInt(this.mileageUnit == null ? -1 : this.mileageUnit.ordinal());
+        dest.writeInt(this.averageDayMileage);
+    }
+
+    private Vehicle(Parcel in) {
+        this.id = in.readInt();
+        this.name = in.readString();
+        this.currentMileage = in.readInt();
+        this.oil = in.readParcelable(Oil.class.getClassLoader());
+        int tmpMileageUnit = in.readInt();
+        this.mileageUnit = tmpMileageUnit == -1 ? null : MileageUnit.values()[tmpMileageUnit];
+        this.averageDayMileage = in.readInt();
+    }
+
+    public static final Parcelable.Creator<Vehicle> CREATOR = new Parcelable.Creator<Vehicle>() {
+        public Vehicle createFromParcel(Parcel source) {
+            return new Vehicle(source);
+        }
+
+        public Vehicle[] newArray(int size) {
+            return new Vehicle[size];
+        }
+    };
 }
