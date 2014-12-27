e35045498d0b01e556dd931ae800c529799d8782
stefanNaumov <stefannaumow@gmail.com>
2014-12-27 16:15:05 +0200

    init views in DetailsActivity

--- OilCheckr/app/src/main/java/eu/artviz/oilcheckr/activities/DetailActivity.java
+++ OilCheckr/app/src/main/java/eu/artviz/oilcheckr/activities/DetailActivity.java
@@ -1,13 +1,25 @@
 package eu.artviz.oilcheckr.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import eu.artviz.oilcheckr.R;
+import eu.artviz.oilcheckr.common.Constants;
+import eu.artviz.oilcheckr.data.dao.HistoryDao;
+import eu.artviz.oilcheckr.models.History;
+import eu.artviz.oilcheckr.models.Vehicle;
 
-public class DetailActivity extends Activity {
+public class DetailActivity extends Activity implements View.OnClickListener{
+
+    private TextView mTvVehicleName, mTvMileage, mTvOilChangeTarget, mTvCurrentOil, mTvAverageMileage;
+    private Button mBtnToUpdate, mBtnToOilChange;
+    private Vehicle mVehicle;
+    private History mHistory;
+    private HistoryDao mHistoryDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,26 +27,37 @@ public class DetailActivity extends Activity {
         setContentView(R.layout.activity_detail);
     }
 
+    private void initViews(){
+        mTvVehicleName = (TextView)findViewById(R.id.tvVehicleName);
+        mTvMileage = (TextView)findViewById(R.id.tvMileage);
+        mTvOilChangeTarget = (TextView)findViewById(R.id.tvOilChangeTarget);
+        mTvCurrentOil = (TextView)findViewById(R.id.tvCurrentOil);
+        mTvAverageMileage = (TextView)findViewById(R.id.tvAverageMileage);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_detail, menu);
-        return true;
+        mBtnToUpdate = (Button)findViewById(R.id.btnToUpdate);
+        mBtnToOilChange = (Button)findViewById(R.id.btnToOilChange);
+
+        mBtnToUpdate.setOnClickListener(this);
+        mBtnToOilChange.setOnClickListener(this);
+    }
+
+    private void init(){
+        mHistoryDao = new HistoryDao(this);
+        mVehicle = (Vehicle)getIntent().getSerializableExtra(Constants.VEHICLE);
+        mTvVehicleName.setText(mVehicle.getName());
+        mTvMileage.setText(String.valueOf(mVehicle.getCurrentMileage()));
+
+        mTvCurrentOil.setText(mVehicle.getOil().getName());
+        mTvAverageMileage.setText(String.valueOf(mVehicle.getAverageDayMileage()));
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        if (v.getId() == mBtnToUpdate.getId()){
+            Intent intent = new Intent(this,UpdateMileageActivity.class);
         }
+        else if(v.getId() == mBtnToOilChange.getId()){
 
-        return super.onOptionsItemSelected(item);
+        }
     }
 }
